ac42f818249ce2d6cbb1e1f829677a75358cad52
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2024-01-13 19:51:31 +0400

    Update arr.java

--- arr.java
+++ arr.java
@@ -1,11 +1,11 @@
-public class arr{
-	public static void main(String[] arg){
-		int[] a=new int[4];
-		a[0]=100;
-		a[1]=120;
-		a[2]=130;
-		a[3]=140;
-			System.out.println(a[1]);
-		
-	}
+public class arr {
+  public static void main(String[] arg) {
+    int[] a = new int[4];
+    a[0] = 100;
+    a[1] = 120;
+    a[2] = 130;
+    a[3] = 140;
+    System.out.println(a[1]);
+
+  }
 }
